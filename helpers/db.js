5110d6d550c2c965c6f65692f747d83d8c0d29c2
Ryan_DIN23SP <ryandilthusha@gmail.com>
2024-03-22 03:33:54 +0200

    created env file in cloud and then add SSL to db.js

--- helpers/db.js
+++ helpers/db.js
@@ -11,6 +11,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.SSL
 });
 
 // A function that accepts SQL query and optional parameters.
